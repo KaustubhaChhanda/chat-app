7dcf35cf24583ec6ab5a93c241fa4d80148b5e93
Kaustubha Chhanda <kaustubhachhandapersonal@gmail.com>
2025-08-11 01:26:07 +0530

    server.js Updated for Vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,14 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
-import bgImage from './assets/bgImage.svg'
 import { Toaster } from 'react-hot-toast'
 import { AuthContext } from '../context/AuthContext'
 
 const App = () => {
   const {authUser} = useContext(AuthContext);
   return (
-    <div className="bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${bgImage})` }}>
-    <Toaster />
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
+      <Toaster />
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to="login" />} />
         <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />}/>
--- server/server.js
+++ server/server.js
@@ -40,5 +40,9 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT: " + PORT + " - http://localhost:" + PORT));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT + " - http://localhost:" + PORT));
+}
+
+export default server;
